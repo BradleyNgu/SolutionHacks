91c9305c52fc987620f90ce5b4f3521feec7154c
jariger <jariger2004@gmail.com>
2025-06-28 14:05:19 -0400

    Base electron app
    Use npm start to run

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,26 @@
-const win = new BrowserWindow({
-  width: 400,
-  height: 600,
-  transparent: true,
-  frame: false,
-  alwaysOnTop: true,
-  resizable: false,
-  webPreferences: {
-    preload: path.join(__dirname, 'preload.js')
-  }
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
 });
